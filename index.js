a9f84bfcac25343bfdad004d1d5086ccc2b2ba1b
enramir <kikeramos9@gmail.com>
2020-02-27 13:29:16 +0000

    final version of l04

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const cool = require("cool-ascii-faces");
+const express = require("express");
 
-console.log(cool());
+var app = express();
+
+app.get("/cool", (request, response) => {
+	response.send("<html>"+cool()+"</html>")
+})
+
+app.listen(8080);
+
+console.log("Server ready");
 
 
